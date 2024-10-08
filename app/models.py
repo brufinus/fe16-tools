5999d0a549de0a9a253c62b77ae074fb9478735d
brufinus <ber.rufinus@gmail.com>
2024-10-08 10:40:19 -0400

    final topic responses should not be unique

--- app/models.py
+++ app/models.py
@@ -70,7 +70,7 @@ class TeaTopic(db.Model):
 class TeaFinalTopic(db.Model):
     id: so.Mapped[int] = so.mapped_column(primary_key=True)
     comment: so.Mapped[str] = so.mapped_column(sa.String(160), index=True, unique=True)
-    response: so.Mapped[str] = so.mapped_column(sa.String(64), index=True, unique=True)
+    response: so.Mapped[str] = so.mapped_column(sa.String(64))
 
     character_id: so.Mapped[int] = so.mapped_column(sa.ForeignKey('character.id'))
 
