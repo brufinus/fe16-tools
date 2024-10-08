02369038d0c76ad45031bc1b55037b356b6369c5
brufinus <ber.rufinus@gmail.com>
2024-10-07 20:28:55 -0400

    show favorite tea

--- app/routes.py
+++ app/routes.py
@@ -3,7 +3,8 @@ from sqlalchemy import func
 
 from app import app, db
 from app.forms import CharacterForm, DualCharacterForm, get_choices
-from app.models import Character, Menu, liked_meals
+from app.models import Character, Menu, liked_meals, Tea, liked_teas
+
 
 @app.route('/')
 @app.route('/index')
@@ -34,8 +35,8 @@ def meal_finder():
     return render_template('meal_finder.html', title='Meal Finder',
                            page_name='Dining Hall Meal Finder', form=form)
 
-@app.route('/get_data', methods=['POST'])
-def get_data():
+@app.route('/get_meal_data', methods=['POST'])
+def get_meal_data():
     cid1 = int(request.json['selected_option1'])
     cid2 = int(request.json['selected_option2'])
 
@@ -69,3 +70,17 @@ def tea_helper():
 
     return render_template('tea_helper.html', title='Tea Helper',
                            page_name='Tea Party Helper', form=form)
+
+@app.route('/get_tea_data', methods=['POST'])
+def get_tea_data():
+    cid = int(request.json['selected_option'])
+
+    char = db.session.get(Character, cid)
+    query = char.likes_tea.select()
+    tea = db.session.scalars(query).all()
+
+    tea_data = [{'tea': t.name} for t in tea]
+    tea_count = len(tea)
+
+    return jsonify({'tea': tea_data,
+                    'tea_count': tea_count})
