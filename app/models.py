c28812186990a7e2d7df3765a658a5d2e1c32647
brufinus <ber.rufinus@gmail.com>
2024-10-10 01:02:49 -0400

    seed model and data

--- app/models.py
+++ app/models.py
@@ -107,3 +107,11 @@ class Gift(db.Model):
 
     def __repr__(self):
         return '<Gift {}>'.format(self.name)
+
+class Seed(db.Model):
+    id: so.Mapped[int] = so.mapped_column(primary_key=True)
+    name: so.Mapped[str] = so.mapped_column(sa.String(64), index=True, unique=True)
+    rank: so.Mapped[int] = so.mapped_column(sa.Integer)
+
+    def __repr__(self):
+        return '<Seed {}>'.format(self.name)
