43668c0996b2f9231a9c09c16a4285fd76f05bc9
brufinus <ber.rufinus@gmail.com>
2024-10-31 22:13:43 -0400

    refactor: implement application factory pattern

--- app/__init__.py
+++ app/__init__.py
@@ -1,36 +1,45 @@
 import logging
+import os
+from logging.handlers import RotatingFileHandler
 
 from flask import Flask
-from config import Config
-from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
-from logging.handlers import RotatingFileHandler
-import os
+from flask_sqlalchemy import SQLAlchemy
 
-app = Flask(__name__)
-app.config.from_object(Config)
-db = SQLAlchemy(app)
-migrate = Migrate(app, db)
+from config import Config
 
-from app.errors import bp as errors_bp
-app.register_blueprint(errors_bp)
 
-from app.tools import bp as tools
-app.register_blueprint(tools, url_prefix='/tools')
+db = SQLAlchemy()
+migrate = Migrate()
 
-from app.main import bp as main
-app.register_blueprint(main)
+def create_app(config_class=Config):
+    app = Flask(__name__)
+    app.config.from_object(config_class)
 
-if not app.debug:
-    if not os.path.exists('logs'):
-        os.mkdir('logs')
-    file_handler = RotatingFileHandler('logs/tools.log', maxBytes=10240, backupCount=10)
-    file_handler.setFormatter(logging.Formatter(
-        '%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d'))
-    file_handler.setLevel(logging.INFO)
-    app.logger.addHandler(file_handler)
+    db.init_app(app)
+    migrate.init_app(app, db)
 
-    app.logger.setLevel(logging.INFO)
-    app.logger.info('Tools startup')
+    from app.errors import bp as errors_bp
+    app.register_blueprint(errors_bp)
+
+    from app.tools import bp as tools
+    app.register_blueprint(tools, url_prefix='/tools')
+
+    from app.main import bp as main
+    app.register_blueprint(main)
+
+    if not app.debug and not app.testing:
+        if not os.path.exists('logs'):
+            os.mkdir('logs')
+        file_handler = RotatingFileHandler('logs/tools.log', maxBytes=10240, backupCount=10)
+        file_handler.setFormatter(logging.Formatter(
+            '%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d'))
+        file_handler.setLevel(logging.INFO)
+        app.logger.addHandler(file_handler)
+
+        app.logger.setLevel(logging.INFO)
+        app.logger.info('Tools startup')
+
+    return app
 
 from app import models
--- tools.py
+++ tools.py
@@ -1,4 +1,6 @@
 import sqlalchemy as sa
 import sqlalchemy.orm as so
-from app import app, db
+from app import create_app, db
 from app.models import Character, Menu
+
+app = create_app()
